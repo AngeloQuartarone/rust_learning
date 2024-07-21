320496b7a8f6a6b460545e196293a8562f3950f5
AngeloQuartarone <quartarone25@gmail.com>
2024-07-21 20:11:45 +0200

    prova

---
+++ hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d
@@ -0,0 +1,5 @@
+/data/uni/rust_learning/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf: src/main.rs
+
+/data/uni/rust_learning/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d: src/main.rs
+
+src/main.rs:
