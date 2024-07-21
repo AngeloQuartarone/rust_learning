d30f126c12195fce7749a6dc384d389505e55ef1
AngeloQuartarone <quartarone25@gmail.com>
2024-07-21 20:10:23 +0200

    prova

--- hello_world/target/debug/deps/hello_world-2778bcc729b47f72.d
+++
@@ -1,5 +0,0 @@
-/home/angelo/data/uni/rust_learning/hello_world/target/debug/deps/libhello_world-2778bcc729b47f72.rmeta: src/main.rs
-
-/home/angelo/data/uni/rust_learning/hello_world/target/debug/deps/hello_world-2778bcc729b47f72.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d
+++
@@ -1,5 +0,0 @@
-/data/uni/rust_learning/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf: src/main.rs
-
-/data/uni/rust_learning/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d: src/main.rs
-
-src/main.rs:
